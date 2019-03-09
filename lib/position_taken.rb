731fc136fa3d17438a3585dfdd1e504796fb0c8c
Bana Malik <bandanakm@gmail.com>
2019-03-09 18:12:20 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 
 def position_taken?(board, index)
   position = board[index]
-  if position == " " || position == "" 
+  if position == " " || position == "" || position == nil
     return false
   end
 
